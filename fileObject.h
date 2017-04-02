8d658c91dd4148536fc3dc0e9c2f50653d4a050e
brooksturtle <brooksturtle@ufl.edu>
2017-04-02 17:40:36 -0400

    Add files via upload
    Classes for each file Object and a memory class that contains a vector of file Objects. Plus a tester class that I used as an interface to test methods in each class.

---
+++ fileObject.h
@@ -0,0 +1,158 @@
+#ifndef FILEOBJECT_H
+#define FILEOBJECT_H
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+
+class fileObject
+{
+    /*
+        fileObject will store two data types.
+        the tagVector will keep all of the tags asscoiated with the object
+        that were added by the user.
+        The fileName is how we will identify the object and and its corresponding
+        file, this will be used to identify it within the memoryVector
+        and also looking for it within the directory
+    */
+    private:
+        vector<string> tagList;
+        string fileName;
+
+    public:
+        fileObject(vector<string>&, string);
+        void addTag(string);
+        bool deleteTag(string);
+        void printTagList();
+        bool checkForTag(string tag);
+        void stringifyFileObject();
+        string returnFileName();
+
+};
+
+// constructor if this is a file with no tags yet
+fileObject::fileObject(vector<string>& newVectorTag, string newFileName)
+{
+    tagList = newVectorTag;
+    fileName= newFileName;
+}
+
+string fileObject::returnFileName()
+{
+    return fileName;
+}
+
+// this class will add a new tag to the vectorTag
+void fileObject::addTag(string tag)
+{
+    tagList.push_back(tag);
+}
+
+/*
+    this will search through the vectorTag for a string called tag
+    and delete it.
+    we will also check if the vector tag is empty, if it is we will
+    will return true so that the memoryVector class can remove the unneeded
+    fileObject
+*/
+// does this tag actually exist, need a safe guard either as a class here or within
+// memoryVector
+bool fileObject::deleteTag(string tag)
+{
+    // search through tag and gets specific index value for the tag
+    int enormity = tagList.size();
+    int index = 0;
+    int counter = 0;
+
+    while (counter < enormity)
+    {
+        if (tagList[counter] == tag)
+        {
+            index = counter;
+        }
+        counter++;
+    }
+    //cout<<"index: "<<index<<endl;
+
+    // need to do the begin thing, index by itself does not work
+    tagList.erase(tagList.begin()+ index);
+
+    // now we will check and return if vectorTag is empty
+    bool isEmpty = false;
+
+    if (tagList.size()== 0)
+    {
+        isEmpty = true;
+    }
+
+    return isEmpty;
+}
+
+// this is going to print out all the tags- duh
+void fileObject::printTagList()
+{
+    int enormity = tagList.size();
+
+    int counter = 0;
+    cout<<fileName<<": ";
+    while (counter < enormity)
+    {
+        // if deals with extra comma at end
+        if (counter == enormity -1)
+        {
+            cout<<tagList[counter];
+            break;
+        }
+        cout<<tagList[counter]<< ", ";
+        counter++;
+
+    }
+    cout<<endl;
+
+}
+
+// this will search through tagList and return true if the tag exist,
+// part of a failsafe to avoid deleting nonexistent tags or adding multiples
+// of the same tag
+bool fileObject::checkForTag(string tag)
+{
+    int enormity = tagList.size();
+    int counter = 0;
+    bool tagExist = false;
+
+    while (counter < enormity)
+    {
+        if (tagList[counter]==tag)
+        {
+            tagExist = true;
+        }
+        counter++;
+    }
+
+    return tagExist;
+}
+
+// add file name and vector tag to a file
+// this will be how we store tags when program is closed
+// on opening, the program will read the file and new fileObjects initialized
+void fileObject::stringifyFileObject()
+{
+    ofstream memoryFile;
+    memoryFile.open ("memoryFile.txt", fstream::in | fstream::out | fstream::app);
+
+    int enormity = tagList.size();
+    int counter = 0;
+
+    memoryFile<<fileName<<": ";
+
+    while (counter< enormity)
+    {
+        memoryFile<<tagList[counter]<<" ";
+        counter++;
+    }
+    memoryFile<<endl;
+    memoryFile.close();
+}
+#endif // FILEOBJECT_H
